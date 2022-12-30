28da5fa4fced2e7b93a4f1556d8880b10196ca7d
Yulia Gudoshnikova <yuvgudoshnikova@edu.hse.ru>
2022-12-30 13:27:11 +0300

    Добавляем Main класс

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+
+    private static final String ROOT_PATH = "./tests/";
+
+    public static void main(String[] args) {
+        System.out.println(ROOT_PATH);
+    }
+}
